ab5cb42d377067ab3f93eb45497a1fa6bf959e1b
czy <chenzhiyuan@mail.ustc.edu.cn>
2020-08-12 00:12:55 +0800

    updata

---
+++ python/database/connect/connect.py
@@ -0,0 +1,124 @@
+import msvcrt
+import time
+
+from ..utils.log import logger
+from ..utils.setting import setting
+
+
+class dbConn(object):
+    _name = "database"
+
+    def __init__(self, host=None, port=None, username=None, password=None):
+        _setting = setting._update_setting(host=host,
+                                           port=port,
+                                           username=username,
+                                           password=password,
+                                           name=self._name)
+        self._host = _setting["host"]
+        self._port = _setting["port"]
+        self._username = _setting["username"]
+        self._password = _setting["password"]
+        self._setting = setting
+
+    @property
+    def _dbDict(self):
+        """
+        :return: the index and name of databases
+        """
+        return {}
+
+    def _connect(self):
+        """
+        the method of connection
+        """
+        return True if hasattr(
+            self, "_is_connected") and self._is_connected else False
+
+    def __len__(self):
+        self._connect()
+        return 0
+
+    def __str__(self):
+        self._connect()
+        host = '"{}"'.format(self._host)
+        user = '"{}"'.format(self._username)
+        baseStr = "<{}(host={} port={} user={})>".format(
+            self._name, host, self._port, user)
+        return baseStr
+
+    __repr__ = __str__
+
+    def __getitem__(self, value):
+        """
+        :param value: value must be int(index of database) or str(name of database)
+        :return (index, value)
+        """
+        if isinstance(value, int):
+            if value < 0:
+                value += self.__len__()
+            if not 0 <= value < self.__len__():
+                raise IndexError("database index out of range")
+            return value, self._dbDict[value]
+        elif isinstance(value, str):
+            for k, v in self._dbDict.items():
+                if v == value:
+                    return k, v
+            return self.__len__(), value
+        else:
+            raise KeyError(
+                "value must be int(index of database) or str(name of database)"
+            )
+
+    _getitem = __getitem__
+
+    def __delitem__(self, value):
+        """
+        flush the database
+        :param value: value must be int(index of database) or str(name of database)
+        :return True: Can flush the database.
+                False: Can't flush the database.
+        """
+        k, v = self._getitem(value)
+        logger.warning("Try to flush database({}:{})!".format(k, v))
+        start_time = time.time()
+        print("Please press any key to continue.")
+        wait_input = ""
+        while True:
+            if msvcrt.kbhit():
+                wait_input = msvcrt.getche()
+            if len(wait_input) != 0 or (time.time() - start_time) > 10:
+                break
+        if len(wait_input) > 0:
+            password = input(
+                "please input the password of the database if you want to flush the database:"
+            )
+            if not self._password or password == self._password:
+                logger.warning("Succeed to flush database({}:{})!".format(
+                    k, v))
+                return True
+        logger.warning("Failed to flush database({}:{})!".format(k, v))
+        return False
+
+    def __setitem__(self, key, value):
+        """
+        rename the database
+        :param key: value must be int(index of database) or str(name of database)
+        :param value: the new name of the database
+        """
+        if not isinstance(value, str):
+            raise KeyError("the new name of the database must be a str")
+        k, v = self._getitem(key)
+        if k == self.__len__():
+            raise IndexError("database index out of range")
+        if value in self._dbDict.values():
+            raise KeyError("The name is already in use.")
+        return k, v
+
+    def __iter__(self):
+        return self._dbDict.__iter__()
+
+    def __call__(self, host=None, port=None, username=None, password=None):
+        return dbConn(host=host,
+                      port=port,
+                      username=username,
+                      password=password)
---
+++ python/database/model/model.py
@@ -0,0 +1,199 @@
+class Model(object):
+    _type = "Model"
+
+    def __init__(self, db, name, **kwargs):
+        """
+        **kwargs: host, port, username, password, info
+        """
+        self._db = db
+        self._name = name
+        self._objectInfo = kwargs
+
+    @property
+    def name(self):
+        return self._name
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        host = '"{}"'.format(self._objectInfo["host"])
+        user = '"{}"'.format(self._objectInfo["username"]
+                             ) if self._objectInfo["username"] else str(
+                                 self._objectInfo["username"])
+        baseStr = '<{}(host={} port={} user={} db={} key="{}")>'.format(
+            self._type, host, self._objectInfo["port"], user,
+            str(self._objectInfo["info"]), self._name)
+        return baseStr
+
+
+class Pair(Model):
+    def __init__(self, db, name, **kwargs):
+        super().__init__(db, name, **kwargs)
+
+    def __len__(self):
+        return super().__len__()
+
+    def __str__(self):
+        return super().__str__()
+
+    def get(self):
+        pass
+
+    def set(self, value):
+        pass
+
+    @property
+    def str(self):
+        pass
+
+    @property
+    def bytes(self):
+        pass
+
+    @property
+    def int(self):
+        pass
+
+    @property
+    def float(self):
+        pass
+
+
+class Sequence(Model):
+    def __init__(self, db, name, **kwargs):
+        super().__init__(db, name, **kwargs)
+
+    def __len__(self):
+        return super().__len__()
+
+    def __str__(self):
+        return super().__str__()
+
+    def __getitem__(self, key):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __contains__(self, value):
+        pass
+
+    def append(self, object):
+        """Append object to the end of the list."""
+        pass
+
+    def extend(self, iterable):
+        """Extend list by appending elements from the iterable."""
+        if not hasattr(iterable, "__iter__"):
+            raise TypeError("{} object is not iterable".format(
+                iterable.__class__.__name__))
+
+    def pop(self, *args):
+        """Remove and return item at index (default last)."""
+        pass
+
+    def clear(self):
+        """Remove all items from list."""
+        pass
+
+    def get(self):
+        """Get all values in the data set."""
+        pass
+
+
+class Container(Model):
+    def __init__(self, db, name, **kwargs):
+        """Initialization"""
+        super().__init__(db, name, **kwargs)
+
+    def __len__(self):
+        """Return the items count of the container."""
+        return super().__len__()
+
+    def __str__(self):
+        """Return the description of the container."""
+        return super().__str__()
+
+    def __getitem__(self, key) -> list or dict:
+        """Find all items that meet the criteria."""
+        if isinstance(key, int):
+            length = self.__len__()
+            if key < 0:
+                key += length
+            if not 0 <= key < length:
+                raise IndexError("list index out of range")
+            return key
+        elif isinstance(key, slice):
+            length = self.__len__()
+            start = key.start or 0
+            stop = key.stop or length
+            step = key.step or 1
+            for i in [start, stop]:
+                i = i if i >= 0 else i + length
+            return slice(start, stop, step)
+        elif isinstance(key, dict):
+            offset = key.get("offset", 0)
+            limit = key.get("limit", 0)
+            for i in [offset, limit]:
+                if not isinstance(i, int):
+                    raise TypeError("{} must be an integer".format(i))
+            return key
+        else:
+            raise TypeError(
+                "list indices must be integers, slices or dicts, not {}".
+                format(key.__class__.__name__))
+
+    def __delitem__(self, key: dict):
+        """Delete all items that meet the criteria."""
+        if not isinstance(key, dict):
+            raise TypeError("list indices must be dicts, not {}".format(
+                key.__class__.__name__))
+
+    def __setitem__(self, key: dict, value: dict):
+        """Modify all items that meet the criteria."""
+        if not isinstance(key, dict):
+            raise TypeError("list indices must be dicts, not {}".format(
+                key.__class__.__name__))
+        if not isinstance(value, dict):
+            raise TypeError("a dict needed to modify the items, not {}".format(
+                key.__class__.__name__))
+
+    def __iter__(self):
+        """Return all items in the container."""
+        pass
+
+    def __next__(self):
+        """Return the next item in the container."""
+        pass
+
+    def append(self, object: dict):
+        """Append object to the end of the list."""
+        if not isinstance(object, dict):
+            raise TypeError("object must be a dict")
+        return dict(object)
+
+    def extend(self, iterable):
+        """Extend list by appending elements from the iterable."""
+        if not hasattr(iterable, "__iter__"):
+            raise TypeError("{} object is not iterable".format(
+                iterable.__class__.__name__))
+        return [dict(i) for i in iterable]
+
+    def pop(self, index: int = -1):
+        """Remove and return item at index (default last)."""
+        pass
+
+    def clear(self):
+        """Remove all items from list."""
+        pass
+
+    def get(self):
+        """Get all values in the data set."""
+        pass
